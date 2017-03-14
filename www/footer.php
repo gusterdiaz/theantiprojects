0852cf44b62f12254d2a5fea4218d4231d9caa0a
g <gusterdiaz@gmail.com>
2017-03-14 14:32:45 -0700

    Added js dependencies

--- www/footer.php
+++ www/footer.php
@@ -1,6 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-<script src="js/script.js"></script>
-
 <footer class="page-footer">
 	<div class="container">
 		<div class="row">
--- www/index.php
+++ www/index.php
@@ -60,6 +60,8 @@
 	</div>
 		
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+	<script src="js/script.js"></script>
 </body>
 
 <?php include('footer.php'); ?>
