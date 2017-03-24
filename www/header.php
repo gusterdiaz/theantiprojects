ce4ed53baf90474de4fde8855a3af39bb2c42804
g <gusterdiaz@gmail.com>
2017-03-24 16:32:12 -0700

    -

--- www/header.php
+++ www/header.php
@@ -6,6 +6,7 @@
 	<meta name="author" content="Gustavo Diaz">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato|Raleway" rel="stylesheet">
 	<link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
 	<link type="text/css" rel="stylesheet" href="css/style.css"/>
 </head>
--- www/js/navbar.js
+++ www/js/navbar.js
@@ -2,7 +2,7 @@ $(document).on('scroll', function(e) { updateColor(); });
 
 function updateColor() {
 	var trans = $(document).scrollTop() / 500;
-	if (trans > 1.000) { trans = 1; }
+	if (trans > 0.99) { trans = 0.99; }
 	var e = $('nav');
 	var currentColor = e.css('background-color');
 	var rgb = currentColor.replace(/^(rgb|rgba)\(/, '').replace(/\)$/, '').replace(/\s/g, '').split(',');
