e9c998c29dc58f9d0270c408d03095dc017057f1
g <gusterdiaz@gmail.com>
2017-04-04 20:00:31 -0700

    -

--- www/header.php
+++ www/header.php
@@ -15,7 +15,7 @@
 		<nav class="nav-color">
 			<div class="nav-wrapper">
 				<ul>
-					<li class="active"><a href="#home">Home</a></li>
+					<li><a class="active" href="#home">Home</a></li>
 					<li><a href="#about">About</a></li>
 					<li><a href="#projects">Projects</a></li>
 					<li><a href="#resume">Resume</a></li>
